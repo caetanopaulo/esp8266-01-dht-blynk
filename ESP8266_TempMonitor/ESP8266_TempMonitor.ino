6da1b0fa6a63c03a52d2e3119f68bf96acae50ff
Paulo Caetano <caetano.paulo@gmail.com>
2020-12-30 20:28:28 +0100

    Adds a wifi manager to the ESP

--- ESP8266_TempMonitor/ESP8266_TempMonitor.ino
+++ ESP8266_TempMonitor/ESP8266_TempMonitor.ino
@@ -1,70 +1,172 @@
-/*************************************************************
-  Download latest Blynk library here:
-    https://github.com/blynkkk/blynk-library/releases/latest
-
-  Blynk is a platform with iOS and Android apps to control
-  Arduino, Raspberry Pi and the likes over the Internet.
-  You can easily build graphic interfaces for all your
-  projects by simply dragging and dropping widgets.
-
-    Downloads, docs, tutorials: http://www.blynk.cc
-    Sketch generator:           http://examples.blynk.cc
-    Blynk community:            http://community.blynk.cc
-    Follow us:                  http://www.fb.com/blynkapp
-                                http://twitter.com/blynk_app
-
-  Blynk library is licensed under MIT license
-  This example code is in public domain.
-
- *************************************************************
-  This example runs directly on ESP8266 chip.
-
-  Note: This requires ESP8266 support package:
-    https://github.com/esp8266/Arduino
-
-  Please be sure to select the right ESP8266 module
-  in the Tools -> Board menu!
-
-  Change WiFi ssid, pass, and Blynk auth token to run :)
-  Feel free to apply it to any other example. It's simple!
- *************************************************************/
-
-/* Comment this out to disable prints and save space */
-#define BLYNK_PRINT Serial
-
-
-#include <ESP8266WiFi.h>
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
 #include <BlynkSimpleEsp8266.h>
+#include <DNSServer.h>
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
+#include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
 #include "DHTesp.h"
+//#define BLYNK_PRINT Serial    // Comment this out to disable prints and save space
+
+#include <Ticker.h>
+Ticker ticker;
 
 DHTesp dht;
+char blynk_token[34] = "YOUR_BLYNK_TOKEN";
+//flag for saving data
+bool shouldSaveConfig = false;
+long humidity;
+long temperature;
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "[BLYNK-TOKEN]";
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "[WIFI-SSID]";
-char pass[] = "[WIFI-PASSWORD]";
-long randNumber;
-
-void setup()
+void tick()
 {
-  // Debug console
-  Serial.begin(9600);
-  dht.setup(2, DHTesp::DHT11);
-  Blynk.begin(auth, ssid, pass);
+  //toggle state
+  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));     // set pin to the opposite state
 }
 
-void loop()
-{
-  //delay(dht.getMinimumSamplingPeriod());
-  delay(3000);
-  float humidity = dht.getHumidity();
-  float temperature = dht.getTemperature();
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
   
+  pinMode(LED_BUILTIN, OUTPUT);
+  ticker.attach(0.6, tick);
+
+  dht.setup(2, DHTesp::DHT11);
+
+  //clean FS, for testing
+  //SPIFFS.format();
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+          strcpy(blynk_token, json["blynk_token"]);
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter custom_blynk_token("blynk", "blynk token", blynk_token, 33);
+
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  //add all your parameters here
+  wifiManager.addParameter(&custom_blynk_token);
+
+  //reset settings - for testing
+  //wifiManager.resetSettings();
+  //ESP.reset();
+
+  //fetches ssid and pass and tries to connect
+  //if it does not connect it starts an access point with the specified name
+  //here  "AutoConnectAP"
+  //and goes into a blocking loop awaiting configuration
+  if (!wifiManager.autoConnect("ESP-DHT-AP")) {
+    Serial.println("failed to connect and hit timeout");
+    ticker.attach(0.2, tick);
+    //reset and try again, or maybe put it to deep sleep
+    ESP.reset();
+    delay(5000);
+  }
+
+  //if you get here you have connected to the WiFi
+  Serial.println("Wifi Connected!...");
+
+  //read updated parameters
+  strcpy(blynk_token, custom_blynk_token.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["blynk_token"] = blynk_token;
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+    json.printTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+
+  //DEBUG
+  //Serial.println("local ip");
+  //Serial.println(WiFi.localIP()); 
+
+  Blynk.config(blynk_token);
+  bool result = Blynk.connect();
+
+  if (result != true)
+  {
+    ticker.attach(0.2, tick);
+    Serial.println("BLYNK Connection Fail");
+    Serial.println(blynk_token);
+    wifiManager.resetSettings();
+    ESP.reset();
+    delay (5000);
+  }
+  else
+  {
+    ticker.attach(0.5, tick);
+    Serial.println("BLYNK Connected");
+    WiFi.mode(WIFI_STA);
+  }
+
+  ticker.detach();
+  //keep LED Off
+  digitalWrite(LED_BUILTIN, HIGH);
+  
+}
+
+
+void loop() {
+  delay(60000);
+  
+  humidity = dht.getHumidity();
+  temperature = dht.getTemperature();
+
   Blynk.run();
   Blynk.virtualWrite(V5, temperature); //sending to Blynk
   Blynk.virtualWrite(V6, humidity); //sending to Blynk
+
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # esp8266-01-dht-blynk
 DHT11 or DHT22 ESP8266-01 shield with blynk and wifi manager
+
+## Hardware
+https://www.aliexpress.com/item/32830838323.html
+
+## How to build 
+Install Arduino IDE: https://www.arduino.cc/en/software
+Install the following libraries:
+- Esp8266: https://github.com/esp8266/Arduino
+- DHT 11: https://github.com/adafruit/DHT-sensor-library
+- blynk
+- arduinojson: https://github.com/bblanchon/ArduinoJson
+- Wifi manager: https://github.com/tzapu/WiFiManager
+
+### Go here are create an account and generate a blynk token
+https://blynk.io/
+
+This project will send:
+- Temperature to V5
+- Humidity to V6
+
+## Others
+
+Resources: 
+https://github.com/IOT-MCU
+https://github.com/tzapu/WiFiManager
+
